cb69f6c605abcb30dacf150f71a2851a5b0d312f
Gabriel Maia Ramos <gamaia@id.uff.br>
2025-09-12 13:48:21 -0300

    movimento basico
    --correção do active input:
    project settings -> player -> configuration -> active input handlling -> Both
    
    --organizacao em funcoes de move() e jump()

--- OLegadoDaOdisseia/Assets/Project/Scripts/Player_Movement.cs
+++ OLegadoDaOdisseia/Assets/Project/Scripts/Player_Movement.cs
@@ -3,21 +3,36 @@ using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
-    public float velocity = 5.0f;
-    public Vector2 movement;
-    
+
+    //Player
+    public float speed = 5.0f;
+    public float jumpForce;
+    public Rigidbody2D rigidBody;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveHori = Input.GetAxis("Horizontal");
-        movement.x = moveHori * velocity * Time.deltaTime;
-        transform.Translate(movement);
+        move();
+        jump();
+    }
+
+    void move() //funcao para movimentacao em x
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        // acessa o componente "transform" do objeto e altera o vetor x e y usando (new Vector2(x,y))
+        gameObject.transform.Translate(new Vector2(HorizontalInput * speed * Time.deltaTime, 0));
+    }
+
+    void jump() //funcao para pulo
+    {
+        // se a tecla space for apertada, altera o valor da velocidade linear do componente RigidBody usando new Vector2(x,y)
+        if (Input.GetKeyDown(KeyCode.Space))
+            rigidBody.linearVelocity = (new Vector2(0, jumpForce));
     }
 }
