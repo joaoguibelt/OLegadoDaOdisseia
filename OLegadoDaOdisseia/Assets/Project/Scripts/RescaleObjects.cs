02692c37a0c50d2ec6e50b393784de0b9368ff26
Gabriel Maia Ramos <gamaia@id.uff.br>
2025-10-29 13:50:41 -0300

    Rescale funcionando E pra qlqr objeto
    rescala com possibilidade de escolher x e y (para fundos) ou so x (para chaos)

---
+++ OLegadoDaOdisseia/Assets/Project/Scripts/RescaleObjects.cs
@@ -0,0 +1,36 @@
+using System.Numerics;
+using UnityEngine;
+
+public class ReescaleObjects : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Camera cam;
+
+    [SerializeField] private bool RenderX;
+    [SerializeField] private bool RenderY;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //largura e altura da camera
+        float camHeight = cam.orthographicSize * 2.0f; //essa funcao retorna a metade da altura
+        //quando vale 5, significa que a altura é de -5 até +5 = 10 //por isso multiplica por 2
+        float camWidth = camHeight * cam.aspect;  //cam aspect retorna largura/altura
+
+        //largura e altura do sprite
+        float spriteHeight = spriteRenderer.sprite.bounds.size.y;
+        float spriteWidth = spriteRenderer.sprite.bounds.size.x;
+
+        UnityEngine.Vector2 newScale = transform.localScale;
+        if (RenderX)
+            newScale.x = camWidth / spriteWidth;
+        if (RenderY)
+            newScale.y = camHeight / spriteHeight;
+
+        transform.localScale = newScale;
+    }
+}
