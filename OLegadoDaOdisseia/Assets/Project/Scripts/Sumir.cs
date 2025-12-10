7406d05697235d0a01b7b9ee9451e56c4ad18c9f
Wuogui <133606772+Wuogui@users.noreply.github.com>
2025-12-10 16:51:41 -0300

    (feat): Maneira mais correta e robusta de sumir com o card (de acordo com o tempo)
    Antes o card estava saindo após o golpe. Agora ele some após alguns segundos.

---
+++ OLegadoDaOdisseia/Assets/Project/Scripts/Sumir.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sumir : MonoBehaviour
+{
+    [SerializeField] Image card;
+    private float timer = 3;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (card.gameObject.activeSelf && timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0)
+        {
+            card.gameObject.SetActive(false);
+            timer = 3;
+        }
+    }
+}
